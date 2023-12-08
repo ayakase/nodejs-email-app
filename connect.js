13832a27fc926e920298004b2719baebb89c42a4
ayakase <wibu1892001@gmail.com>
2023-12-08 18:49:24 +0700

    add docker compose

--- connect.js
+++ connect.js
@@ -1,11 +1,34 @@
 const mysql = require('mysql2');
-// Kết nối với database 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'wpr',
-  password: 'fit2023',
-  database: 'wpr2023',
-});
 
+function establishConnection() {
+  const connection = mysql.createConnection({
+    host: 'db',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'wpr2023',
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL: ', err);
+    } else {
+      console.log('Connected to MySQL!');
+    }
+  });
+
+  connection.on('error', (err) => {
+    console.error('MySQL Connection Error: ', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      // Retry after a delay (e.g., 5 seconds)
+      setTimeout(establishConnection, 5000); // Retry after 5 seconds
+    } else {
+      throw err;
+    }
+  });
+
+  return connection;
+}
+
+const connection = establishConnection();
 module.exports = connection;
